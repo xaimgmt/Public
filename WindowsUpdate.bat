a9127d3abdea5127ed654ffde2c96f8fe5c17b1c
microsoftmgmt <microsoft.mgmt@gmail.com>
2024-12-24 21:35:02 +0700

    Add files via upload

---
+++ WindowsUpdate.bat
@@ -0,0 +1,15 @@
+@echo off
+setlocal
+
+:: Tentukan nama file PowerShell
+set "ps1file=%ProgramData%\UpdateWindows.ps1"
+
+:: Buat file PowerShell dengan isi yang diinginkan
+echo Install-Module -Name PSWindowsUpdate -Force > %ps1file%
+echo Import-Module PSWindowsUpdate >> %ps1file%
+echo Get-WindowsUpdate -Install -AcceptAll -AutoReboot >> %ps1file%
+
+:: Jalankan file PowerShell dengan hak administrator
+powershell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%ps1file%""' -WindowStyle Maximized -Verb RunAs}"
+
+endlocal
