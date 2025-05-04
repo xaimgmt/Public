e120c6a9a6f335e4067924c4289edc99fc72dfd6
microsoftmgmt <microsoft.mgmt@gmail.com>
2025-05-04 11:37:29 +0700

    Update Format FD.py

--- Format FD.py
+++ Format FD.py
@@ -1,7 +1,21 @@
 
 import os
 import time
+import psutil
+import sys
 os.system("cls")
+
+def detect_udf_drives():
+    udf_drives = []
+    for partition in psutil.disk_partitions():
+        if partition.fstype.lower() == "udf":
+            udf_drives.append(partition.device)
+    return udf_drives
+
+udf_drives = detect_udf_drives()
+udf_drives_str = ", ".join(udf_drives).replace("['", "").replace("']", "")
+print("UDF Drives Detected:", udf_drives_str)
+# sys.exit()
 
 def format_flash_drive(disk_number):
     commands = [
@@ -29,7 +43,8 @@ for i in range(1, 6):
 for drive_letter in range(ord('D'), ord('P') + 1):
     if chr(drive_letter) == 'D':
         continue
-    os.system(f'robocopy /mir C: {chr(drive_letter)}:')
+    # os.system(f'robocopy /mir C: {chr(drive_letter)}:')
+    os.system(f'robocopy /mir {udf_drives_str}\ {chr(drive_letter)}:\\')
 
 # for drive_letter in ['E', 'G', 'I', 'K']:
 #     os.system(f'robocopy /mir D: {drive_letter}:')
