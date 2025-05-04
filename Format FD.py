c557bb6dae232c54bc2d2c1a704ba5ba67f329b1
microsoftmgmt <microsoft.mgmt@gmail.com>
2025-05-04 11:25:54 +0700

    Update Format FD.py

--- Format FD.py
+++ Format FD.py
@@ -1,18 +1,38 @@
+
 import os
+import time
+os.system("cls")
 
 def format_flash_drive(disk_number):
     commands = [
-        f"diskpart /s /c \"select disk {disk_number}\"",
+        f"select disk {disk_number}",
         "clean",
         "create partition primary",
         "format fs=ntfs quick",
-        "assign"
+        "assign",
+        "active"
     ]
-    for command in commands:
-        os.system(f"echo {command} | diskpart")
+    script_path = "C:\\Temp\\dp.txt"
+    with open(script_path, "w") as script_file:
+        script_file.write("\n".join(commands))
+    os.system(f"diskpart /s {script_path}")
 
 # Format flash drives located at disk 1 to disk 5
 for i in range(1, 6):
     format_flash_drive(i)
 
-print("Flash drives from disk 1 to disk 5 have been formatted to NTFS successfully.")
+time.sleep(3)
+
+for i in range(1, 6):
+    format_flash_drive(i)
+
+for drive_letter in range(ord('D'), ord('P') + 1):
+    if chr(drive_letter) == 'D':
+        continue
+    os.system(f'robocopy /mir C: {chr(drive_letter)}:')
+
+# for drive_letter in ['E', 'G', 'I', 'K']:
+#     os.system(f'robocopy /mir D: {drive_letter}:')
+
+print("DONE")
+
