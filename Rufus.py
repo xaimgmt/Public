f8ff454b6db57da8dd76103a6e89333b169f9565
xaimgmt <microsoft.mgmt@gmail.com>
2025-07-11 21:25:14 +0700

    Add files via upload

--- Rufus.py
+++ Rufus.py
@@ -36,21 +36,25 @@ def WinGetColor(nama_jendela, koordinat_X, koordinat_Y, kode_warna):
     else:
         raise Exception(f"Jendela dengan nama '{nama_jendela}' tidak ditemukan.")
 
-versi_rufus = '4.9.2256'
-for i in range(3):
+versi_rufus = 'Rufus 4.9.2256'
+for i in range(2):
     print(i + 1, end=' ')
     while True:
-        WinClick(f'Rufus {versi_rufus}', 306, 488) # START
+        if WinGetColor(versi_rufus, 95, 442, (6, 176, 37)):
+            WinClick(versi_rufus, 412, 486) # CANCEL
+            s(1); WinClick('Rufus - Cancellation', 268, 137)
+            s(1); WinClick('Cancelled', 407, 124)
+            s(3); WinClick(versi_rufus, 238, 103) # Device
+            s(1); pyautogui.hotkey('down') # Pilih USB
+            s(1); pyautogui.hotkey('enter') # Enter
+            s(1)
+            break
+
+        WinClick(versi_rufus, 306, 488) # START
         WinClick('Revoked UEFI bootloader detected', 276, 243)
         WinClick('Rufus', 277, 150)
         WinClick('Rufus - Cancellation', 268, 137)
         WinClick('Cancelled', 407, 124)
-
-        if WinGetColor(f'Rufus {versi_rufus}', 95, 442, (6, 176, 37)):
-            WinClick(f'Rufus {versi_rufus}', 412, 486) # CANCEL
-            s(1); WinClick('Rufus - Cancellation', 268, 137)
-            s(1); WinClick('Cancelled', 407, 124)
-            break
 
         time.sleep(1)
 
