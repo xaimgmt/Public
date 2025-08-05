df5485534478a221f99b23e20d14aa86a270a756
xaimgmt <microsoft.mgmt@gmail.com>
2025-08-05 14:22:03 +0700

    Add files via upload

--- Rufus.py
+++ Rufus.py
@@ -1,6 +1,6 @@
 
 import sys
-sys.path.append('C:\\Program Files\\Python313\\Lib')
+sys.path.append(r'\\10.234.219.222\Py$\Lib')
 import pyautogui
 import time
 import pygetwindow as gw
@@ -49,11 +49,19 @@ s(0.5); pyautogui.hotkey('alt', 'd')
 s(0.5); pyautogui.typewrite('This PC')
 s(0.5); pyautogui.hotkey('enter')
 
-os.system('start "" /min D:\\rufus-4.9.exe -i D:\\Wipe_2025.iso -f NTFS')
+CN = os.environ.get('COMPUTERNAME')
+if CN == 'PMIIDIDNW03648':
+    LokasiFileISO = 'A:\\ISO\\Original\\WipeOri11 2505 for User.iso'
+    DriveLast = 'X'
+elif CN == 'PMIIDIDNW04472':
+    LokasiFileISO = 'A:\\ISO\\WipeOri11 2503 for User.iso'
+    DriveLast = 'Z'
+
+os.system(f'start "" /min C:\\ProgramData\\rufus-4.9.exe -i "{LokasiFileISO}" -f NTFS')
 s(5)
 
 versi_rufus = 'Rufus 4.9.2256'
-for i in range(2):
+for i in range(10):
     print(i + 1, end=' ')
     while True:
         if WinGetColor(versi_rufus, 95, 442, (6, 176, 37)):
@@ -66,6 +74,9 @@ for i in range(2):
             s(1)
             break
 
+        if WinClick('Error', 402, 124):
+            sys.exit()
+
         WinClick(versi_rufus, 306, 488) # START
         WinClick('Revoked UEFI bootloader detected', 276, 243)
         WinClick('Rufus', 277, 150)
@@ -77,18 +88,18 @@ for i in range(2):
 s(3); os.system('taskkill /f /im rufus-4.9.exe')
 
 for i in range(5):
-    os.system(r"powershell Dismount-DiskImage -ImagePath 'D:\Wipe_2025.iso'")
+    os.system(f"powershell Dismount-DiskImage -ImagePath '{LokasiFileISO}'")
 
-os.system(r"powershell Mount-DiskImage -ImagePath 'D:\Wipe_2025.iso'")
-for drive_letter in range(ord('E'), ord('Z') + 1):
+os.system(f"powershell Mount-DiskImage -ImagePath '{LokasiFileISO}'")
+for drive_letter in range(ord('D'), ord(DriveLast) + 1):
     if os.path.exists(f"{chr(drive_letter)}:\\WINSETUP\\Win11\\Windows11.iso"):
         Src = chr(drive_letter)
 
-for drive_letter in range(ord('E'), ord('Z') + 1):
+for drive_letter in range(ord('D'), ord(DriveLast) + 1):
     if os.path.exists(f"{chr(drive_letter)}:\\WINSETUP"):
         Des = chr(drive_letter)
         if Des != Src:
-            os.system(f'title Refill FD Wipe Ori - Src {Src}: & color e & robocopy /mir {Src}:\\ {Des}:\\')
+            os.system(f'title Refill FD Wipe Ori - Src {Src}: Des {Des}: & color e & robocopy /mir {Src}:\\ {Des}:\\')
 
 sys.exit()
 
